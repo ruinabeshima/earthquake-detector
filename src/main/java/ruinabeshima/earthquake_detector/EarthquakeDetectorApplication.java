dc02753219dfbc570829b159230358a1d5ede117
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-06-18 14:14:29 +0900

    initialized files

---
+++ src/main/java/ruinabeshima/earthquake_detector/EarthquakeDetectorApplication.java
@@ -0,0 +1,13 @@
+package ruinabeshima.earthquake_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruinabeshima/earthquake_detector/EarthquakeDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ruinabeshima.earthquake_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
